47f155c29daea9d4c132e145d1a72d0674547f1a
Seneca <flabbergasted242@gmail.com>
2021-08-03 18:40:35 +0300

    Initial Commit

---
+++ SpringAnimationAppTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimationAppTraining
+//
+//  Created by Дмитрий Дмитрий on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
